b31921c02db1f15f194c433318fcaed54637937e
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-18 17:45:20 +0900

    Initial commit

---
+++ app/src/main/java/app/babachan/l4s_imageview/MainActivity.kt
@@ -0,0 +1,15 @@
+package app.babachan.l4s_imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.pascal2)
+    }
+}
