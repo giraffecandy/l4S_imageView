43b7602ba57d537fc462b871aa5093aa75d53ef2
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-18 17:54:19 +0900

    urlで画像表示

--- app/src/main/java/app/babachan/l4s_imageview/MainActivity.kt
+++ app/src/main/java/app/babachan/l4s_imageview/MainActivity.kt
@@ -3,13 +3,16 @@ package app.babachan.l4s_imageview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import coil.api.load
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val url = "https://i.pinimg.com/originals/49/9c/bb/499cbbd5bc465b4a5d83afd94e3ed8ab.jpg"
         val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageResource(R.drawable.pascal2)
+
+        imageView.load(url)
     }
 }
